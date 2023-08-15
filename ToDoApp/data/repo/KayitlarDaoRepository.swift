3ffb4153210980b271e17e8848b767a787853e9e
IŞIL VARDARLI <isilvardarli@ISILs-MacBook-Air.local>
2023-08-15 14:57:25 +0300

    Fourth Commit

--- ToDoApp/data/repo/KayitlarDaoRepository.swift
+++ ToDoApp/data/repo/KayitlarDaoRepository.swift
@@ -12,32 +12,96 @@ class KayitlarDaoRepository{
     
     var kayitListesi = BehaviorSubject<[Kayitlar]>(value: [Kayitlar]())
     
+    let db:FMDatabase?
+    init() {
+      let dosyaYolu = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+      let veritabaniURL = URL(fileURLWithPath: dosyaYolu).appendingPathComponent("toDoDb.sqlite")
+        db = FMDatabase(path: veritabaniURL.path)
+    }
+    
+    
     func kaydet(kayit_icerik : String){
-        print("Kaydedilen:  \(kayit_icerik)")
+        db?.open()
+        do{
+        try db!.executeUpdate("INSERT INTO kayitlar(kayit_icerik ) VALUES (?)", values:[kayit_icerik])
+        }
+        catch{
+            print(error.localizedDescription)
+            }
+        db?.close()
         
     }
     
     func guncelle(kayit_id:Int,kayit_icerik:String) {
-        print("Güncellendi : \(kayit_id)-\(kayit_icerik)")
+        db?.open()
+        do{
+        try db!.executeUpdate("UPDATE kayitlar SET kayit_icerik = ? WHERE kayit_id = ?", values:[ kayit_icerik ,kayit_id ])
+        }
+        catch{
+            print(error.localizedDescription)
+            }
+        db?.close()
     }
     
     func listele() {
+      
+
+        db?.open()
         var liste = [Kayitlar]()
-        let k1 = Kayitlar(kayit_id: 1,kayit_icerik: "su iç")
-        let k2 = Kayitlar(kayit_id: 2,kayit_icerik: "dondurma al")
-        let k3 = Kayitlar(kayit_id: 3,kayit_icerik: "mama sipariş et")
-        liste.append(k1)
-        liste.append(k2)
-        liste.append(k3)
-        kayitListesi.onNext(liste)
+        do{
+            let result = try db!.executeQuery("SELECT * FROM kayitlar", values: nil)
+            while result.next(){
+                let kayit_id = Int(result.string(forColumn: "kayit_id"))!
+                let kayit_icerik = result.string(forColumn: "kayit_icerik")!
+                                      
+                let kayit = Kayitlar(kayit_id: kayit_id,kayit_icerik: kayit_icerik)
+                liste.append(kayit)
+                                                   
+            }
+            kayitListesi.onNext(liste)
+        }
+        catch{
+            print(error.localizedDescription)
+             
+        }
+        
+        db?.close()
+       
     }
     
     func sil(kayit_id:Int) {
-        print("Kayıt sil: \(kayit_id)")
+        db?.open()
+        do{
+        try db!.executeUpdate("DELETE FROM kayitlar WHERE kayit_id = ?", values:[kayit_id])
+        }
+        catch{
+            print(error.localizedDescription)
+            }
+        db?.close()
     }
     
     func ara(searchText:String) {
-        print("Madde Ara: \(searchText)")
+        db?.open()
+        var liste = [Kayitlar]()
+        do{
+            let result = try db!.executeQuery("SELECT * FROM kayitlar WHERE kayit_icerik LIKE '%\(searchText)%'", values: nil)
+            while result.next(){
+                let kayit_id = Int(result.string(forColumn: "kayit_id"))!
+                let kayit_icerik = result.string(forColumn: "kayit_icerik")!
+                                      
+                let kayit = Kayitlar(kayit_id: kayit_id,kayit_icerik: kayit_icerik)
+                liste.append(kayit)
+                                  
+                 
+            }
+            kayitListesi.onNext(liste)//tetikleme
+        }
+        catch{
+            print(error.localizedDescription)
+             
+        }
+        
+        db?.close()
     }
     
     
--- ToDoApp/ui/viewmodel/AnasayfaViewModel.swift
+++ ToDoApp/ui/viewmodel/AnasayfaViewModel.swift
@@ -13,6 +13,7 @@ class AnasayfaViewModel{
     var kayitRepo = KayitlarDaoRepository()
     var kayitListesi = BehaviorSubject<[Kayitlar]>(value: [Kayitlar]())
     init() {
+        veritabaniKopyala()
         kayitListesi = kayitRepo.kayitListesi
     }
     func listele() {
@@ -21,9 +22,35 @@ class AnasayfaViewModel{
     }
     func sil(kayit_id:Int) {
         kayitRepo.sil(kayit_id: kayit_id)
+        kayitRepo.listele()
     }
     func ara(searchText:String) {
         kayitRepo.ara(searchText: searchText)
+    }
+    
+    func veritabaniKopyala(){
+        let bundleYolu = Bundle.main.path(forResource: "toDoDb", ofType: ".sqlite")
+        let dosyaYolu = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let veritabaniURL = URL(fileURLWithPath: dosyaYolu).appendingPathComponent("toDoDb.sqlite")
+        
+        let fm = FileManager.default
+        
+        if fm.fileExists(atPath: veritabaniURL.path() ){
+           print("veritabanı zaten var")
+           
+        }
+        else{
+          do{
+              try fm.copyItem(atPath: bundleYolu! , toPath: veritabaniURL.path)
+              
+          }
+          catch{
+                kayitRepo.listele()
+               
+                  }
+            
+        }
+        
     }
     
 }
